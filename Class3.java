edaa4bbdce8c50e7e67d43f1676f241a34c2ff2d
Eric Wade <ewade@stu.jsu.edu>
2020-08-19 17:50:38 -0500

    Added Class3

---
+++ Class3.java
@@ -0,0 +1,7 @@
+public class Class3 {
+	private void f1() {
+		// Placeholder method (v1.0)	
+	}
+private void f2() {// Placeholder method (v2.0)}
+private void f3() {// Placeholder method (v2.0)}
+}
